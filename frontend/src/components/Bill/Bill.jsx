32fe27023c639f889941fd1b06420428ac287e1e
tblackwellaurora <tblackwell@aurorasolar.com>
2024-02-19 23:19:39 -0500

    added all reps voting NAY and office details

--- frontend/src/components/Bill/Bill.jsx
+++ frontend/src/components/Bill/Bill.jsx
@@ -1,47 +1,100 @@
-import {PropTypes} from "prop-types"
-import {useState, useEffect} from "react"
-import { useParams } from "react-router-dom"
-
-const RelatedBills = ({related}) => {
-  if (!related) return <div>No related bills</div>
-  return (
-    <div>
-      {Object.values(related).map((bill) => (<div key={bill.printNo}><a href={`/bill/${bill.session}/${bill.printNo}`}>{bill.printNo}</a>: {bill.title}</div>))}
-    </div>
-  );
-  }
-RelatedBills.propTypes = {
-  related: PropTypes.object.isRequired,
-};
+import { PropTypes } from "prop-types";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./Bill.css";
 
 export const Bill = () => {
-  const {sessionYear, printNo} = useParams();
+  const { sessionYear, printNo } = useParams();
   const [bill, setBill] = useState();
+  const [nayMembersData, setNayMembersData] = useState([]);
 
   useEffect(() => {
-    const fetchBill = async() => {
+    const fetchBill = async () => {
       const res = await fetch(`/api/v1/bills/${sessionYear}/${printNo}`);
-      await setBill(await res.json());
+      const billData = await res.json();
+      setBill(billData);
+    };
+    fetchBill();
+  }, [sessionYear, printNo]);
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      const nayMembers = bill?.votes?.items[bill.votes.items.length - 1]?.memberVotes?.items?.NAY?.items || [];
+      const promises = nayMembers.map(async member => {
+        const res = await fetch(`https://v3.openstates.org/people?jurisdiction=New%20York&district=${member.districtCode}&include=offices&page=1&per_page=1&apikey=7747347d-782f-43d1-b3e5-8c4bed578a27
+        `);
+        const data = await res.json();
+        return data.results[0];
+      });
+      const nayMembersData = await Promise.all(promises);
+      setNayMembersData(nayMembersData);
+    };
+
+    if (bill) {
+      fetchMembers();
     }
-    fetchBill()
-  }, [])
+  }, [bill]);
 
-  if (!bill) return (
-    <div>
-      <div>This is the bill {printNo}&apos;s page</div>
-    </div>
-  )
+  if (!bill) {
+    return (
+      <div>
+        <div>This is the bill {printNo}&apos;s page</div>
+      </div>
+    );
+  }
 
-  const {title, summary, sponsor: {member: {memberId: sponsorId, fullName: sponsorName}}} = bill;
+  const {
+    title,
+    summary,
+    sponsor: {
+      member: { fullName: sponsorName },
+    },
+  } = bill;
 
   return (
     <div>
-      <div>This is the bill {printNo}&apos;s page</div>
-      <h2>{title}</h2>
-      <p>{summary}</p>
-      <div><span>{sponsorName}:{sponsorId}</span></div>
-      <RelatedBills related={bill.billInfoRefs.items} />
+      <div className="summary">
+        <h2>{title}</h2>
+        <p>{summary}</p>
+        <p>
+          Sponsored by {sponsorName} | District{" "}
+          {bill.sponsor.member.districtCode}
+        </p>
+        <p>Status: {bill.status.statusDesc}</p>
+      </div>
+      <div className="action">
+        <h2>Take Action!</h2>
+        <p>Members currently AGAINST this bill:</p>
+        <ul>
+        { nayMembersData.map(member => (
+          <li key={member.id}>
+            {member.name}:{" "}
+            {member.offices.map(office => (
+              <div key={office.address}>
+                <a href={`tel:${office.voice}`}>
+                  {office.voice}
+                </a> - {office.address.split(',').slice(-2)[0].trim()}
+              </div>
+            ))}
+          </li>
+        ))}
+        </ul>
+        <div className="Script">
+          <h4>Script</h4>
+          <p>
+            Hi, my name is [NAME] and I\'m a constituent from [CITY, ZIP].
+            <br />
+            <br />
+            I'm calling to urge [REP/SEN NAME] to vote YES on {printNo}.
+            <br />
+            Thank you for your time and consideration.
+            <br />
+            <br />
+            IF LEAVING A VOICEMAIL: Please leave your full street address to
+            ensure your call is tallied.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-
-}
+  );
+};
