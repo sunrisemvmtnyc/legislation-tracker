56b93280798ffa9c9f7c73acfd7b9ba7c4aaf295
Derek <derek.miranda.1992@gmail.com>
2024-03-04 17:13:47 -0500

    lint: set eslint rules
    since current codebase mostly in single-quotes,
    using semicolons at end of lines,
    and using 2-spaced indent,
    want to encourage that w/ eslint
    
    also disable react/prop-types invalidation

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,25 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "jest/globals": true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'jest'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 0,
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
   },
 };
