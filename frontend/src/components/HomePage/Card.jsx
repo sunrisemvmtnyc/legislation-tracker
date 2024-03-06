b6beeff4f87837544aff94c5cf6cdb59d317e383
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-03-06 17:34:56 -0500

    fix: Handle double-mount/request in dev/strict mode

--- frontend/src/components/HomePage/Card.jsx
+++ frontend/src/components/HomePage/Card.jsx
@@ -28,6 +28,7 @@ const Card = ({ bill }) => {
           <div className='home-bill-categories'>
             {
               categories.map(category => <CategoryTag category={category} key={category} />)
+              // categories.map(category => <CategoryTag category={category} key={`${bill.basePrintNoStr}:${category}`} />)
             }
           </div>
         )
