d12f97d83e862756da8edbbb56af4dbb68b01164
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-03-11 13:18:37 -0400

    linting changes

--- backend/src/nysenate-api.js
+++ backend/src/nysenate-api.js
@@ -1,36 +1,34 @@
 import fetch from 'node-fetch';
 
-export const URL_BASE = "https://legislation.nysenate.gov/api/3";
+export const URL_BASE = 'https://legislation.nysenate.gov/api/3';
 
 export const legApi = (path, params = {}) => {
-
   // Add API key to all requests
   params.key = process.env.OPEN_LEGISLATION_KEY;
   // console.log("API URL to string", `${URL_BASE}${path}` + "?" + (new URLSearchParams(params)).toString())
-  return `${URL_BASE}/${path}` + "?" + (new URLSearchParams(params)).toString();
-
+  return `${URL_BASE}/${path}` + '?' + new URLSearchParams(params).toString();
 };
 
-const fetchAllPages = async(apiPath, firstResponseData) => {
+const fetchAllPages = async (apiPath, firstResponseData) => {
   let allItems = firstResponseData.result.items;
   const totalPages = Math.ceil(firstResponseData.total / 1000);
   for (let i = 1; i < totalPages; i++) {
-    let offsetStart = (i * 1000) + 1;
-    let nextResponse = await fetch(legApi(apiPath, {offset: offsetStart}));
+    let offsetStart = i * 1000 + 1;
+    let nextResponse = await fetch(legApi(apiPath, { offset: offsetStart }));
     let nextResponseData = await nextResponse.json();
     allItems = allItems.concat(nextResponseData.result.items);
   }
-  return allItems
-}
+  return allItems;
+};
 
-export const billsFromYear = async(year) => {
+export const billsFromYear = async (year) => {
   // FIXME: legacy, delete
   // First request with no offset
   let firstResponse = await fetch(legApi(`bills/${year}`));
   let firstResponseData = await firstResponse.json();
 
   if (!firstResponseData.success) {
-    throw('Did not successfully retrieve bills from legislation.nysenate.gov. Response from API was marked as a failure.');
+    throw 'Did not successfully retrieve bills from legislation.nysenate.gov. Response from API was marked as a failure.';
   }
 
   // Retrieve the remaining pages
