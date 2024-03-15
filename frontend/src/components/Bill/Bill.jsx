eb0978d94a46e161d7de4c24e4fc2b0ecb5532e9
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-03-14 21:07:36 -0400

    chore: lint/prettier autofixes

--- frontend/src/components/Bill/Bill.jsx
+++ frontend/src/components/Bill/Bill.jsx
@@ -1,53 +1,77 @@
-import {PropTypes} from "prop-types";
-import {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import { PropTypes } from 'prop-types';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const RelatedBills = ({related}) => {
+const RelatedBills = ({ related }) => {
   if (!related) return <div>No related bills</div>;
   return (
     <div>
-      {Object.values(related).map((bill) => (<div key={bill.printNo}><a href={`/bill/${bill.session}/${bill.printNo}`}>{bill.printNo}</a>: {bill.title}</div>))}
+      {Object.values(related).map((bill) => (
+        <div key={bill.printNo}>
+          <a href={`/bill/${bill.session}/${bill.printNo}`}>{bill.printNo}</a>:{' '}
+          {bill.title}
+        </div>
+      ))}
     </div>
   );
-  };
+};
 RelatedBills.propTypes = {
   related: PropTypes.object.isRequired,
 };
 
 function getSponsorNumber(billData) {
-  const { activeVersion, amendments: { items } } = billData;
-  return 1 + items[activeVersion].coSponsors.size + items[activeVersion].multiSponsors.size;
+  const {
+    activeVersion,
+    amendments: { items },
+  } = billData;
+  return (
+    1 +
+    items[activeVersion].coSponsors.size +
+    items[activeVersion].multiSponsors.size
+  );
 }
 
 export const Bill = () => {
-  const {sessionYear, printNo} = useParams();
+  const { sessionYear, printNo } = useParams();
   const [bill, setBill] = useState();
 
   useEffect(() => {
-    const fetchBill = async() => {
+    const fetchBill = async () => {
       const res = await fetch(`/api/v1/bills/${sessionYear}/${printNo}`);
       await setBill(await res.json());
     };
     fetchBill();
   }, []);
 
-  if (!bill) return (
-    <div>
-      <div>This is the bill {printNo}&apos;s page</div>
-    </div>
-  );
+  if (!bill)
+    return (
+      <div>
+        <div>This is the bill {printNo}&apos;s page</div>
+      </div>
+    );
 
-  const {title, summary, sponsor: {member: {memberId: sponsorId, fullName: sponsorName}}} = bill;
+  const {
+    title,
+    summary,
+    sponsor: {
+      member: { memberId: sponsorId, fullName: sponsorName },
+    },
+  } = bill;
 
   return (
     <div>
       <div>This is the bill {printNo}&apos;s page</div>
       <h2>{title}</h2>
       <p>{summary}</p>
-      <div><span>{sponsorName}:{sponsorId}</span></div>
-      <div><span>Total Sponsors: {getSponsorNumber(bill)}</span></div>
+      <div>
+        <span>
+          {sponsorName}:{sponsorId}
+        </span>
+      </div>
+      <div>
+        <span>Total Sponsors: {getSponsorNumber(bill)}</span>
+      </div>
       <RelatedBills related={bill.billInfoRefs.items} />
     </div>
   );
-
 };
