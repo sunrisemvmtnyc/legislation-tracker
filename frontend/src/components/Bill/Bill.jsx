aacedd1b82b42e47e97785e6541a6110d8b3e24f
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-03-22 14:08:07 -0400

    Show bill committee members

--- frontend/src/components/Bill/Bill.jsx
+++ frontend/src/components/Bill/Bill.jsx
@@ -32,10 +32,49 @@ RelatedBills.propTypes = {
   related: PropTypes.object.isRequired,
 };
 
+const BillCommitteeMembers = ({ committee, memberId, isSenate }) => {
+  // TODO: better styling
+  if (!isSenate) return <div>Assembly committees not currently supported</div>;
+  if (!committee) return <div>No committee data</div>;
+  console.log('committee');
+  console.log(committee);
+  const members = committee.committeeMembers.items.filter(
+    (m) => m.memberId !== memberId
+  );
+
+  return (
+    <div>
+      <h4>Committee: {committee.name} </h4>
+      <div>
+        {members.map((member) => (
+          <div key={member.memberId}>
+            <span>
+              {member.fullName}:{member.memberId}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <span>Total Committee Members: {members.length}</span>
+      </div>
+    </div>
+  );
+};
+BillCommitteeMembers.propTypes = {
+  committee: PropTypes.object.isRequired,
+  memberId: PropTypes.string.isRequired,
+  isSenate: PropTypes.bool.isRequired,
+};
+
 export const Bill = () => {
   const { sessionYear, printNo } = useParams();
   const [bill, setBill] = useState();
+  const [committee, setCommittee] = useState();
 
+  const fetched = !!bill;
+  const isSenate = bill?.billType?.chamber?.toLowerCase() === 'senate';
+
+  // Fetch main bill data
   useEffect(() => {
     const fetchBill = async () => {
       const res = await fetch(`/api/v1/bills/${sessionYear}/${printNo}`);
@@ -43,6 +82,23 @@ export const Bill = () => {
     };
     fetchBill();
   }, []);
+
+  // Fetch bill committee data
+  useEffect(() => {
+    if (!fetched || !isSenate) return;
+    const chamber = bill.billType.chamber.toLowerCase();
+    const committee = bill?.status?.committeeName;
+    if (!committee) return;
+
+    const fetchCommittee = async () => {
+      const res = await fetch(
+        `/api/v1/committees/${sessionYear}/${chamber}/${committee}`
+      );
+      setCommittee(await res.json());
+    };
+
+    fetchCommittee();
+  }, [bill, sessionYear]);
 
   if (!bill)
     return (
@@ -73,6 +129,13 @@ export const Bill = () => {
         <span>Total Sponsors: {getSponsorNumber(bill)}</span>
       </div>
       <RelatedBills related={bill.billInfoRefs.items} />
+      {fetched && (
+        <BillCommitteeMembers
+          committee={committee}
+          memberId={sponsorId}
+          isSenate={isSenate}
+        />
+      )}
     </div>
   );
 };
