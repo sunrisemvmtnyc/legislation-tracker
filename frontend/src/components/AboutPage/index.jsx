9eda150bae1bb5e925509801ef07900b9f0e4680
Aurélien Greffard <agr@witbe.net>
2024-05-23 18:44:05 -0400

    Wrong variable for description. I'm tired

--- frontend/src/components/AboutPage/index.jsx
+++ frontend/src/components/AboutPage/index.jsx
@@ -5,17 +5,17 @@ const blocks = [
   {
     title: "our goal",
     question: "What is the NY State Legislative Tracker?",
-    descripton: "The NY State Legislative Tracker is a tool designed to keep you informed about key legislative activities in New York State. We focus on selected bills that we, along with our partner organizations, think are important and need attention right now. Our tracker provides real-time updates and detailed information on these important bills, making it easier for you to stay engaged and involved in the legislative process.",
+    description: "The NY State Legislative Tracker is a tool designed to keep you informed about key legislative activities in New York State. We focus on selected bills that we, along with our partner organizations, think are important and need attention right now. Our tracker provides real-time updates and detailed information on these important bills, making it easier for you to stay engaged and involved in the legislative process.",
   },
   {
     title: "use solution",
     question: "How to use the NY State Legislative Tracker?",
-    descripton: "We believe that citizen involvement is crucial to making a difference. Contacting your local representatives and letting them know your stance on these bills can put significant pressure on them to act. Your voice can help push important legislation forward. Don't underestimate the power of your participation! Reach out, make your opinions known, and help us make a change.",
+    description: "We believe that citizen involvement is crucial to making a difference. Contacting your local representatives and letting them know your stance on these bills can put significant pressure on them to act. Your voice can help push important legislation forward. Don't underestimate the power of your participation! Reach out, make your opinions known, and help us make a change.",
   },
   {
     title: "about us",
     question: "Who are we?",
-    descripton: "Sunrise NYC’s digital tools team and legislative team are collaborating to pull together this NY State Legislative Tracker. We are dedicated to fighting against climate change and advocating for social justice. We want to make it easier for you to understand what’s going on in the state legislature and how you can get involved. Together, we can make sure our voices are heard and make a real impact on the laws that affect us all.",
+    description: "Sunrise NYC’s digital tools team and legislative team are collaborating to pull together this NY State Legislative Tracker. We are dedicated to fighting against climate change and advocating for social justice. We want to make it easier for you to understand what’s going on in the state legislature and how you can get involved. Together, we can make sure our voices are heard and make a real impact on the laws that affect us all.",
   }
 ];
 
@@ -23,11 +23,11 @@ function AboutPage() {
   return (
     <div id="about-page">
       {
-        blocks.map(({ title, question, descripton }) => (
+        blocks.map(({ title, question, description }) => (
           <div class="about-page-block">
             <span className="about-page-block-title">{title}</span>
             <span className="about-page-block-question">{question}</span>
-            <span className="about-page-block-desc">{question}</span>
+            <span className="about-page-block-desc">{description}</span>
           </div>
         ))
       }
