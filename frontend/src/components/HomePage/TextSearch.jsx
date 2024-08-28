79b62be921799ecbe555b6519a3b524d2d13822b
Derek <derek.miranda.1992@gmail.com>
2024-08-28 15:07:34 -0400

    feat(search): initial text input

---
+++ frontend/src/components/HomePage/TextSearch.jsx
@@ -0,0 +1,28 @@
+import { useMemo } from 'react';
+import { TextField, debounce } from '@mui/material';
+import PropTypes from 'prop-types';
+
+import { SEARCH_DEBOUNCE_TIME } from '../../constants';
+import './TextSearch.css';
+
+const TextSearch = ({ updateValue }) => {
+  const handleChange = useMemo(
+    () =>
+      debounce((event) => {
+        updateValue(event.target.value);
+      }, SEARCH_DEBOUNCE_TIME),
+    [updateValue]
+  );
+
+  return (
+    <div className="text-input-container">
+      <TextField id="text-input" label="Search" onChange={handleChange} />
+    </div>
+  );
+};
+
+TextSearch.propTypes = {
+  updateValue: PropTypes.func.isRequired,
+};
+
+export default TextSearch;
