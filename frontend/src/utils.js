92f0e4b4160b110ea4fa5220bbff99ec6cf3aee9
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-08-29 08:17:01 -0400

    fix: Allow getSponsorMembers to handle bills with no cosponsors

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,11 +1,24 @@
+/** Get sponsors for bills
+ *
+ * @param {Array} bills - Array of bill objects from legislative API
+ * @returns {{
+ *    sponsor: {
+ *      memberId: number,
+ *      chamber:	"ASSEMBLY" | "SENATE",
+ *      incumbent: boolean,
+ *      fullName: string,
+ *      shortName: string,
+ *      imgName: string,
+ *      alternate:	boolean,
+ *      sessionMemberId: number,
+ *      sessionYear: number,
+ *      districtCode: number,
+ * }, coSponsors: Array<Object | undefined>
+ * }} - Object with sponsor and co-sponsors
+ */
 export function getSponsorMembers(billsResult) {
-  const {
-    sponsor: { member: sponsorMember },
-    coSponsors: { items: coSponsorMembers },
-  } = billsResult;
-
   return {
-    sponsor: sponsorMember,
-    coSponsors: coSponsorMembers,
+    sponsor: billsResult?.sponsor?.member,
+    coSponsors: billsResult?.coSponsors?.items,
   };
 }
