63cf3d332a23b505069961be719e6fecd968fa9a
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-10-02 11:33:48 -0400

    chore: Inline header content

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 import sunriseImage from '../../assets/sunrisenyc.png';
@@ -8,14 +8,20 @@ function Header() {
     <div className="header-container">
       <Link to="/" className="header-title">
         <img src={sunriseImage} alt="Sunrise logo" height="90px" width="90px" />
+        <div>NY State&nbsp;&nbsp;legislative tracker</div>
       </Link>
-      <div className="header-actions">
-        <a className="header-get-involved" href="https://sunrisenyc.org/get-involved" target="_blank" rel="noreferrer">Get involved</a>
-        <div className="header-navbar">
-          {/* <Link to="/learn">Learn</Link> */}
-          <Link to="/advanced">Advanced view</Link>
-          <Link to="/about">About</Link>
-        </div>
+      <div className="header-navbar">
+        {/* <Link to="/learn">Learn</Link> */}
+        <Link to="/advanced">Advanced view</Link>
+        <Link to="/about">About</Link>
+        <a
+          className="header-get-involved"
+          href="https://sunrisenyc.org/get-involved"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Get involved
+        </a>
       </div>
     </div>
   );
